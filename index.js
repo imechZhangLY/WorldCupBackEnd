b8eb030354373e1220923686f0d299d5ae41c60a
张路姚 <zhangluyao@zhangluyaodeMacBook-Pro.local>
2018-06-05 10:56:18 +0800

    将公网ip修改为局域网ip

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.set('port', process.env.PORT || 80);
-app.set('host', process.env.HOST || '0.0.0.0')
+app.set('host', process.env.HOST || '172.16.0.15')
 
 const port = app.get('port'),
 	ip = app.get('host')
