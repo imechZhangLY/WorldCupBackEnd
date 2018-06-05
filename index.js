80cc29fd2ecb65033ae05aaf9b2197e4177ea7df
张路姚 <zhangluyao@zhangluyaodeMacBook-Pro.local>
2018-06-05 10:45:18 +0800

    修改ip地址错误

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
-const ip = '139.199.10.189',
-	port = 80
+app.set('port', process.env.PORT || 8080);
+app.set('host', process.env.HOST || '139.199.10.189')
+
+const port = app.get('port'),
+	ip = app.get('host')
+
+console.log(port)
+console.log(ip)
 
 app.use(express.static('public'))
 
